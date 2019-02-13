66d1534bbe18c510976aaa6ca9150b57e45d0216
SnehaParuchuri <47022196+SnehaParuchuri@users.noreply.github.com>
2019-02-13 11:51:42 +0530

    Update dockerise.sh

--- webAppExample/dockerise.sh
+++ webAppExample/dockerise.sh
@@ -4,3 +4,5 @@ docker login -u santoshm93 -p Internity@123
 echo $BUILD_ID
 docker build --no-cache -f Dockerfile -t santoshm93/mavenwebapp:$BUILD_ID .
 docker push santoshm93/mavenwebapp:$BUILD_ID
+dcos auth login --username root --password Caas@123
+dcos marathon app remove /mvn-app
