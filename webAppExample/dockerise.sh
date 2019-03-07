df1e2588657e8323fda9baf97c4c9a9dfbf54e9d
SnehaParuchuri <47022196+SnehaParuchuri@users.noreply.github.com>
2019-03-07 12:56:21 +0530

    Update dockerise.sh

--- webAppExample/dockerise.sh
+++ webAppExample/dockerise.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-docker login -u santoshm93 -p Internity@123
+
 echo $BUILD_ID
 docker build --no-cache -f Dockerfile -t santoshm93/mavenwebapp:$BUILD_ID .
 docker push santoshm93/mavenwebapp:$BUILD_ID
